a052a989fedb70eb873d3ba36468fc5a769851e2
Ray <raymachead66@gmail.com>
2020-11-23 16:52:03 -0500

    made them have stuff

--- src/main/java/com/quakec/http/RegisterMemberRequest.java
+++ src/main/java/com/quakec/http/RegisterMemberRequest.java
@@ -1,5 +1,34 @@
 package com.quakec.http;
 
 public class RegisterMemberRequest {
+	String arg1;
+	String arg2;
+	String arg3;
 
+	
+	public String getArg1() { return arg1;}
+	public void setArg1(String a) {this.arg1 = a;}
+	
+	public String getArg2() { return arg2;}
+	public void setArg2(String a) {this.arg2 = a;}
+	
+	public String getArg3() { return arg3;}
+	public void setArg3(String a) {this.arg3 = a;}
+	
+	public String toString() {
+		return "Add(" + arg1 + "," + arg2 + "," + arg3 + ")";
+	}
+	
+	public RegisterMemberRequest(String arg1, String arg2) {
+		this.arg1 = arg1;
+		this.arg2 = arg2;
+		this.arg3 = "";
+	}
+	
+	public RegisterMemberRequest(String arg1, String arg2, String arg3) {
+		this.arg1 = arg1;
+		this.arg2 = arg2;
+		this.arg3 = arg3;
+	}
+	
 }
--- src/main/java/com/quakec/http/RegisterMemberResponse.java
+++ src/main/java/com/quakec/http/RegisterMemberResponse.java
@@ -1,5 +1,24 @@
 package com.quakec.http;
 
 public class RegisterMemberResponse {
-
+	public int statusCode;
+	public String error;
+	
+	public RegisterMemberResponse (int statusCode) {
+		this.statusCode = statusCode;
+		this.error = "";
+	}
+	
+	public RegisterMemberResponse (int statusCode, String errorMessage) {
+		this.statusCode = statusCode;
+		this.error = errorMessage;
+	}
+	
+	public String toString() {
+		if (statusCode / 100 == 2) {  // too cute?
+			return "It worked!";
+		} else {
+			return "ErrorResult(" + statusCode + ", err=" + error + ")";
+		}
+	}
 }
