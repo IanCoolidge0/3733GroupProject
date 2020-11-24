e44166d43ae48793c4f6b8212bec61be2fa7566a
Ian Coolidge <ian.coolidge@asdnh.org>
2020-11-24 00:03:03 -0500

    alternative model = pretty simple

---
+++ src/main/java/com/quakec/model/Alternative.java
@@ -0,0 +1,33 @@
+package com.quakec.model;
+
+import java.util.UUID;
+
+public class Alternative {
+
+	public final String name;
+	public final String id;
+	public final int number;
+	
+	public Alternative(String name, String id, int number) {
+		this.name = name;
+		this.id = id;
+		this.number = number;
+	}
+	
+	public Alternative(String name, int number) {
+		this(name, UUID.fromString("alternative:"+name+number).toString(), number);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+	
+}
