90f7ffb5cb47f79349cb21c8c4316d33aa869121
Ray <raymachead66@gmail.com>
2020-11-24 17:00:59 -0500

    fixed html and added javascript for reigster user

--- js/api.js
+++ js/api.js
@@ -3,4 +3,4 @@
 var base_url = "https://exe5sj3z1c.execute-api.us-east-2.amazonaws.com/development/";
 
 var createchoice_url = base_url + "createchoice";   // POST
- 
+var registeruser_url = base_url + "registeruser";   // POST
---
+++ js/registeruser.js
@@ -0,0 +1,34 @@
+function handleRegisterMemberClick(e) {
+	var form = document.registerMemberForm;
+
+	var choiceid = form.choiceid.value;
+	var name = form.name.value;
+	var password = form.password.value;
+
+
+	var data = {};
+	data["choiceId"] = choiceid;
+	data["name"] = name;
+	data["password"] = password;
+
+
+
+	var js = JSON.stringify(data);
+	console.log("JS: " + js);
+
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", registeruser, true);
+
+	// send the collected data as JSON
+	xhr.send(js);
+
+	// This will process results and update HTML as appropriate.
+	xhr.onloadend = function () {
+		console.log(xhr);
+		console.log(xhr.request);
+
+		if(xhr.readyState == XMLHttpRequest.DONE) {
+			console.log("XHR: " + xhr.responseText);
+		}
+	}
+}
