713e15366a60a5197b547a9178eb57e720449cdc
Ray <raymachead66@gmail.com>
2020-11-24 20:37:37 -0500

    embedded js in html register member, any characters after url become default value for choice id

--- js/createchoice.js
+++ js/createchoice.js
@@ -41,5 +41,5 @@ function handleCreateChoiceClick(e) {
 			console.log("XHR: " + xhr.responseText);
 		}
 	}
-	window.location.replace(https://3733quakec.s3.us-east-2.amazonaws.com/presentations/registeruser.html);
+	//window.location.replace('https://3733quakec.s3.us-east-2.amazonaws.com/presentations/registeruser.html?'+choiceid);
 }
