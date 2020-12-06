8e9002d865141001f3b15e3e079c8653316e5244
raymondcarter <raymondcarter@autoreg-183925.dyn.wpi.edu>
2020-12-05 21:10:17 -0500

    added request

--- src/main/java/com/quakec/http/ViewChoicesRequest.java
+++ src/main/java/com/quakec/http/ViewChoicesRequest.java
@@ -1,5 +1,25 @@
 package com.quakec.http;
 
 public class ViewChoicesRequest {
+	String id;
+	
+	public String getId() {return id;}
+	
+	public void setId(String id) {this.id=id;}
+	
+	public String toString() {
+		String s = "ViewChoice( " + id;
+		return s;
+	}
+	
+	public ViewChoicesRequest() {
+		
+	}
+	
+	public ViewChoicesRequest(String id) {
+		this.id = id;
+	}
+	
+	
 
 }
