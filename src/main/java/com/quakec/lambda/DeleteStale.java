826aa4a88257aac6fd37c3508bb89547fc6af64f
Ian Coolidge <ian.coolidge@asdnh.org>
2020-12-11 17:52:09 -0500

    fix

--- src/main/java/com/quakec/lambda/DeleteStale.java
+++ src/main/java/com/quakec/lambda/DeleteStale.java
@@ -2,19 +2,11 @@ package com.quakec.lambda;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.S3Event;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.GetObjectRequest;
-import com.amazonaws.services.s3.model.S3Object;
 import com.quakec.db.ChoicesDAO;
 import com.quakec.http.DeleteStaleRequest;
 import com.quakec.http.DeleteStaleResponse;
-import com.quakec.http.ProduceReportRequest;
-import com.quakec.http.ProduceReportResponse;
-import com.quakec.model.Choice;
-
-import java.util.List;
 
 public class DeleteStale implements RequestHandler<DeleteStaleRequest, DeleteStaleResponse> {
 	private AmazonS3 s3 = AmazonS3ClientBuilder.standard().build();
@@ -32,7 +24,7 @@ public class DeleteStale implements RequestHandler<DeleteStaleRequest, DeleteSta
         DeleteStaleResponse response;
 
         try {
-            if (deleteStale(request.getTime())) {
+            if (deleteStale(request.getTimeInMills())) {
                 response = new DeleteStaleResponse();
             } else {
                 response = new DeleteStaleResponse(403, "Delete Stale Failed");
