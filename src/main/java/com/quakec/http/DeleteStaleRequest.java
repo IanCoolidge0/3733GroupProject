11ae9f993c4541f2db2912ef5872bd91c1cbeab6
KyleWynnM <74930526+KyleWynnM@users.noreply.github.com>
2020-12-10 19:59:00 -0500

    Updated getters and setters

--- src/main/java/com/quakec/http/DeleteStaleRequest.java
+++ src/main/java/com/quakec/http/DeleteStaleRequest.java
@@ -3,11 +3,20 @@ package com.quakec.http;
 public class DeleteStaleRequest {
 
     private int timeInMills;
+
+    public DeleteStaleRequest() {
+
+    }
+
     public DeleteStaleRequest(int timeInMills) {
         this.timeInMills = timeInMills;
     }
 
-    public int getTime() {
+    public void setTimeInMills(int timeInMills) {
+        this.timeInMills = timeInMills;
+    }
+
+    public int getTimeInMills() {
         return this.timeInMills;
     }
 }
